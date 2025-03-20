d692672024d5aa305b7523ccc3e93010cc5d257a
BrunoDiaz54 <ivanreyes2709@hotmail.com>
2025-03-19 23:22:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por ponerte en contacto con nosotros. Te responderemos pronto.');
+});

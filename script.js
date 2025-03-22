3d2c9625c27802154703b557daa2fb2c867e8e88
BrunoDiaz54 <ivanreyes2709@hotmail.com>
2025-03-21 20:17:45 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Lógica para mostrar mensaje de confirmación al enviar el formulario
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Gracias por ponerte en contacto con nosotros. Te responderemos pronto.');
